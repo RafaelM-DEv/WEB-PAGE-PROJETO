e4aeef27c3202b27244640a81bc03bbfcb4ee41f
Far3llProject <42079080+Far3llProject@users.noreply.github.com>
2019-06-03 11:56:28 -0300

    Avanço no projeto
    Acrecentei uma parte para futuros projetos desenvolvidos em aula, breve farei outra pagina para eles

---
+++ WEB PAGE TRABALHO/script/index.js
@@ -0,0 +1,2 @@
+alert("Bem vindo ao site do meu projeto interdiciplinar, esse site está sendo desenvolvido de acordo com o curso,  ainda a elementos que estão por vir, desde já agradeço os feedbacks! - Rafael Martins")
+
